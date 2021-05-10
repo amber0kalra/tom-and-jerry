960b756cc8a5fe9ee35492282d2d555d976d91a2
amber0kalra <78156575+amber0kalra@users.noreply.github.com>
2021-05-10 19:12:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var bgImg
+var cat, catImg1, catImg2, catImg3;
+var mouse, mouseImg1, mouseImg2, mouseImg3;
+
+
+function preload() {
+    //load the images here
+    bgImg = loadImage("garden.png");
+    catImg1 = loadImage("cat1.png");
+    catImg2 = loadAnimation("cat2.png","cat3.png");
+    catImg3 = loadImage("cat4.png");
+    mouseImg1 = loadImage("mouse1.png");
+    mouseImg2 = loadAnimation("mouse2.png","mouse3.png");
+    mouseImg3 = loadImage("mouse4.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create cat and mouse sprites here
+cat = createSprite(870,600);
+cat.addAnimation("catSleeping",catImg1);
+cat.scale = 0.2;
+
+mouse = createSprite(200,600);
+mouse.addAnimation("mouseStanding",mouseImg1);
+mouse.scale = 0.15;
+
+}
+
+function draw() {
+
+    background(bgImg);
+    //Write condition here to evalute if cat and mouse collide
+
+    if( cat.x - mouse.x < (cat.width - mouse.width)/2){
+        cat.velocityX= 0;
+        cat.addAnimation("catLastImg", catImg3)
+        cat.x = 300;
+        cat.scale = 0.2;
+        //cat.velocityX= 0;
+        mouse.changeAnimation("mouseLastImg")
+        mouse.addAnimation("mouseLastImg", mouseImg3)
+        //mouse.x = 300;
+        mouse.scale = 0.15;
+        mouse.changeAnimation("mouseLastImg")
+    }
+
+    drawSprites();
+
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+  if(keyCode === LEFT_ARROW){ 
+    cat.velocityX = -5; 
+    cat.addAnimation("catRunning", catImg2); 
+    cat.changeAnimation("catRunning"); 
+    mouse.addAnimation("mouseTeasing", mouseImg2); 
+    mouse.frameDelay = 25; 
+    mouse.changeAnimation("mouseTeasing"); 
+}
+}
+
+
